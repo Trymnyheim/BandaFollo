233239b42b1a2cddaa23ebf22e38ccdf78f79a2b
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-06-21 20:36:12 +0200

    add: dropdown in navbar

--- BF/src/App.jsx
+++ BF/src/App.jsx
@@ -8,27 +8,26 @@ import OmOss from './pages/OmOss.jsx';
 import Kontakt from './pages/Kontakt.jsx';
 import Tjenester from './pages/Tjenester.jsx';
 import Produkter from './pages/Produkter.jsx';
-import BlaaResept from './pages/BlaaResept.jsx';
 
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <Navigation />
       <div className="content">
-        <BrowserRouter>
           <Routes>
             <Route index path='/' element={<Home />} />
             <Route path='/omoss' element={<OmOss />} />
             <Route path='/kontakt' element={<Kontakt />} />
             <Route path='/produkter' element={<Produkter />} />
             <Route path='/tjenester' element={<Tjenester />} />
-            <Route path='/blaaresept' element={<BlaaResept />} />
           </Routes>
-        </BrowserRouter>
+        
       </div>
       <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- BF/src/components/Navigation.jsx
+++ BF/src/components/Navigation.jsx
@@ -1,9 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Shop from '../components/Shop.jsx';
 import { useState } from 'react';
-import BackToTop from './BackToTop.jsx';
+import { HashLink } from 'react-router-hash-link';
+
+function scrollWithOffset(el) {
+    const yOffset = -300;
+    const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+}
 
 function Navigation(openShop) {
 
@@ -11,24 +18,39 @@ function Navigation(openShop) {
     const handleCloseShop = () => setTimeout(() => setShowShop(false), 0);
     const handleShowShop = () => setShowShop(true);
 
+    const [expanded, setExpanded] = useState(false);
+
+    const handleSelect = () => {
+        setExpanded(false); // Collapse navbar after selection
+    };
+
     return (
         <>
-            <Navbar expand="md" sticky="top" bg="light" data-bs-theme="light">
+            <Navbar expanded={expanded} onToggle={setExpanded} expand="lg" sticky="top" bg="light" data-bs-theme="light">
                 <Container>
                     <Navbar.Brand href="/">
                         <img className="logo" src="./src/assets/logo.png"/>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse>
-                        <Nav className="me-auto">
+                        <Nav onSelect={handleSelect} className="me-auto" >
                             {/*
                             <Nav.Link href="/"><svg xmlns="http://www.w3.org/2000/svg" stroke="grey" fill="grey" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path></svg></Nav.Link>
                             */}
                             <Nav.Link href="/omoss">OM OSS</Nav.Link>
                             <Nav.Link href="/kontakt">KONTAKT</Nav.Link>
-                            <Nav.Link href="/tjenester">TJENESTER</Nav.Link>
+                            <NavDropdown title="TJENESTER" id="basic-nav-dropdown">
+                                <NavDropdown.Item onClick={handleSelect} as={HashLink} smooth to="/tjenester#hjemmebesok" scroll={scrollWithOffset}>
+                                    HJEMMEBESØK
+                                </NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleSelect} as={HashLink} smooth to="/tjenester#levering" scroll={scrollWithOffset}>
+                                    LEVERING
+                                </NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleSelect} as={HashLink} smooth to="/tjenester#veiledning" scroll={scrollWithOffset}>
+                                    RÅD OG VEILEDNING
+                                </NavDropdown.Item>
+                            </NavDropdown>
                             <Nav.Link href="/produkter">PRODUKTER</Nav.Link>
-                            <Nav.Link href="/blaaresept">BLÅRESEPT</Nav.Link>
                             <Nav.Link onClick={handleShowShop}>NETTBUTIKK</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
