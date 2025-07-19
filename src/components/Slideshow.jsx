3a9d03dc8956d17b9f68a9de21d402ba3fe53bcf
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-20 00:01:17 +0200

    fix: move style to .css, code cleanup

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,123 +1,67 @@
 import { useState, useEffect } from 'react';
+import './components.css';
 
 function Slideshow(props) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [showArrows] = useState(
-            props.withArrows && (props.images?.length || 0) > 1
-        );
+        props.withArrows && (props.images?.length || 0) > 1
+    )
 
-    const slideshowStyle = {
-        position: 'relative',
-        overflow: 'hidden',
-        width: `${props.width ? props.width + 'vh': '100%'}`,
-        height: `${props.height ? props.height + 'vh': '100%'}`
-    };
-
-    const imageStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundSize: props.contain ? 'contain': 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        height: '100%',
-        width: '100%',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        transition: 'opacity 1s ease-in-out',
-        opacity: 0,
-    };
-
-    const activeImageStyle = {
-        ...imageStyle,
-        opacity: 1,
-    };
-
-    const overlayStyle = {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        pointerEvents: 'none',
-        zIndex: 10,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: '48px',
-    };
-
-    const arrowStyle = {
-        position: 'absolute',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        fontSize: '2rem',
-        color: 'white',
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
-        border: 'none',
-        padding: '0.5rem 1rem',
-        cursor: 'pointer',
-        zIndex: 20,
-    };
-
-    const leftArrowStyle = {
-        ...arrowStyle,
-        left: '10px',
-    };
-
-    const rightArrowStyle = {
-        ...arrowStyle,
-        right: '10px',
-    };
+    const slideshowSize = {
+        width: props.width ? props.width + 'vh' : '100%',
+        height: props.height ? props.height + 'vh' : '100%',
+    }
 
     const goToPrevious = () => {
         setCurrentIndex((prevIndex) =>
             (prevIndex - 1 + props.images.length) % props.images.length
-        );
-    };
+        )
+    }
 
     const goToNext = () => {
         setCurrentIndex((prevIndex) =>
             (prevIndex + 1) % props.images.length
-        );
-    };
+        )
+    }
 
     useEffect(() => {
         if (!props.time) return;
+
         const interval = setInterval(() => {
             goToNext();
         }, props.time);
 
         return () => clearInterval(interval);
-    }, [props.images.length, props.time]);
+    }, [props.images.length, props.time])
 
     return (
-        <div style={slideshowStyle}>
+        <div className="slideshow" style={slideshowSize}>
             {props.images.map((image, index) => (
                 <div
                     key={index}
-                    style={{
-                        ...(index === currentIndex
-                            ? activeImageStyle
-                            : imageStyle),
-                        backgroundImage: `url(${image.url})`,
-                    }}
+                    className={
+                        `slideshow-image 
+                        ${index === currentIndex ? 'active' : ''} 
+                        ${props.contain ? 'contain' : 'cover'}`
+                    }
+                    style={{ backgroundImage: `url(${image.url})` }}
                 />
             ))}
-            {showArrows &&
+            {showArrows && (
                 <>
-                    <button style={leftArrowStyle} onClick={goToPrevious}>
+                    <button className="slideshow-arrow left" onClick={goToPrevious}>
                         ‹
                     </button>
-                    <button style={rightArrowStyle} onClick={goToNext}>
+                    <button className="slideshow-arrow right" onClick={goToNext}>
                         ›
                     </button>
                 </>
-            }
-            <div style={overlayStyle}>
-                {props.overlay && props.overlay}
-            </div>
+            )}
+            {props.overlay && (
+                <div className="slideshow-overlay">{props.overlay}</div>
+            )}
         </div>
-    );
+    )
 }
 
 export default Slideshow;
