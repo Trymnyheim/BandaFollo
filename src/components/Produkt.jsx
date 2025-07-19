7b1e560e3711a0ce7ec72a94384e35cb61ca04f7
Trymnyheim <trym.haakon.nyheim@gmail.com>
2025-07-19 23:21:46 +0200

    fix: image aspect ratio, indents

--- src/components/Produkt.jsx
+++ src/components/Produkt.jsx
@@ -3,43 +3,39 @@ import useLinkify from '/src/utils/useLinkify.jsx';
 import { Fragment } from 'react';
 
 function Produkt({ produkt, id, children}) {
-  
-  const linkedParagraphs = produkt.about.map((p) => useLinkify(p));
-  const linkedUnderText = produkt.underText ? produkt.underText.map((p) => useLinkify(p)) : [];
 
-  return (
-    <div className="container" id={id}>
-      <div className="produkt-text-container">
-        <h1>{produkt.title}</h1>
-        {linkedParagraphs.map((linked, index) => (
-          <Fragment key={index}>{linked}</Fragment>
-        ))}
+    const linkedParagraphs = produkt.about.map((p) => useLinkify(p));
+    const linkedUnderText = produkt.underText ? produkt.underText.map(
+                            (p) => useLinkify(p)) : [];
 
-        <ul>
-          {produkt.subProducts.map((subProduct, index) => (
-            <li key={index}>{subProduct}</li>
-          ))}
-        </ul>
-
-        {linkedUnderText.map((linked, index) => (
-          <Fragment key={index}>{linked}</Fragment>
-        ))}
-
-      </div>
-      <div className="produkt-image-container">
-        <Slideshow
-          images={produkt.images}
-          time={1000000}
-          withArrows={true}
-          contain
-        />
-      </div>
-      <div className="container justify-center padding">
-          {children}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container produkt" id={id}>
+            <div className="produkt-text-container">
+                <h1>{produkt.title}</h1>
+                {linkedParagraphs.map((linked, index) => (
+                    <Fragment key={index}>{linked}</Fragment>
+                ))}
+                <ul>
+                    {produkt.subProducts.map((subProduct, index) => (
+                        <li key={index}>{subProduct}</li>
+                    ))}
+                </ul>
+                {linkedUnderText.map((linked, index) => (
+                    <Fragment key={index}>{linked}</Fragment>
+                ))}
+            </div>
+            <div className="produkt-image-container">
+                <Slideshow
+                images={produkt.images}
+                withArrows={true}
+                contain
+                />
+            </div>
+            <div className="container justify-center padding">
+                {children}
+            </div>
+        </div>
+    )
 }
-
 
 export default Produkt;
