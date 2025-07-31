d36931f7d5b005d7709d3fd3ab27f8f3bc62374c
trymhnyheim <trym.haakon.nyheim@gmail.com>
2025-07-31 17:06:20 +0200

    add: Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import HolidayProvider from './utils/HolidayContext.jsx';
+import { AuthProvider } from './utils/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <HolidayProvider>
       <App />
     </HolidayProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
---
+++ src/utils/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+      localStorage.setItem('userData', JSON.stringify(userData));
+      setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('userData');
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('userData');
+    if (storedUser) {
+      const parsed = JSON.parse(storedUser);
+
+      fetch('http://localhost:5000/user/validate', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${parsed.accessToken}`,
+        },
+      })
+        .then(res => {
+          if (!res.ok) throw new Error('Token invalid or expired');
+          return res.json();
+        })
+        .then(data => {
+          setUser(parsed); 
+        })
+        .catch(() => {
+          logout(); // Remove invalid token
+        });
+    }
+  }, []);
+
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
